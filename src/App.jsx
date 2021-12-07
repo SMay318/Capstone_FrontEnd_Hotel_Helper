ffe977d9cd04dcf8a9b91cc6eb59a57b6a2233d7
Sean May <s_may318@hotmail.com>
2021-12-07 14:29:45 -0500

    updated folders and installed necessary attachments to begin working on react, as well as added in my APP component to start building out my login and register page.

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import jwtDecode from 'jwt-decode';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loggedInUser: null,
+      issues: [],
+      comments: []
+    }
+  }
+
+  componentDidMount() {
+        
+    const jwt = localStorage.getItem('token');
+    try {
+        const user = jwtDecode(jwt);
+        this.setState({loggedInUser: user });
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+registerNewUser = async (user) => {
+  console.log("User object from Register: ", user)
+  try{
+      const response = await axios.post('https://localhost:44394/api/authentication', user);
+      console.log(response)
+      this.loggedInUser = ({'userName': user.userName, 'password': user.password})
+      window.location = ('/Login')
+
+  }
+  catch(error) {
+      console.log(error, 'Invalid input');
+  }
+}
+
+
+
+loginUser = async (login) => {
+  console.log("User object from login:", login)
+  try {
+      let response = await axios.post('https://localhost:44394/api/authentication/login', login);
+      this.setState({
+          user: response.data.token
+      });
+      this.setState({
+          jwt: response.data.token
+      });
+      localStorage.setItem('token', response.data.token);
+      
+      window.location = ('/Home')
+
+  } catch (error) {
+      alert('Invalid username or password')
+  }
+
+}
+
+  render(){
+    <div>
+
+    </div>
+  }
+
+
+
+
+
+
+
+}
+
+
+export default App;
